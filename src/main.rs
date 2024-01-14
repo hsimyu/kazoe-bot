d29543a2182a1c7bf74c7fc4e83854c89d5054ab
hsimyu <hsimyu1900@gmail.com>
2024-01-14 16:27:35 +0900

    Event に対する反応を行えるようにした.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,48 @@
 use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
+use serenity::{
+    all::GatewayIntents,
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
 use std::env;
 use tokio;
 
-#[derive(Serialize)]
-struct Message {
-    content: String,
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if !msg.author.bot {
+            println!("Received message: {}", msg.content);
+
+            if msg.content.contains("hello bot") {
+                if let Err(why) = msg.reply(&ctx.http, "Hello!").await {
+                    println!("Error replying: {:?}", why);
+                }
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv::dotenv().ok();
+async fn main() {
+    dotenv().ok();
 
     let bot_token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN must be set in .env");
-    let channel_id = env::var("TARGET_CHANNEL_ID").expect("TARGET_CHANNEL_ID must be set in .env");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    // let channel_id = env::var("TARGET_CHANNEL_ID").expect("TARGET_CHANNEL_ID must be set in .env");
 
-    let message_content = "Hello!";
-    let api_url = format!(
-        "https://discord.com/api/v10/channels/{}/messages",
-        channel_id
-    );
+    let mut client = Client::builder(bot_token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
 
-    let message = Message {
-        content: message_content.to_string(),
-    };
-
-    let client = reqwest::Client::new();
-    let response = client
-        .post(&api_url)
-        .header("Authorization", format!("Bot {}", bot_token))
-        .header("Content-Type", "application/json")
-        .json(&message)
-        .send()
-        .await?;
-
-    if response.status().is_success() {
-        println!("Message sent successfully!");
-    } else {
-        println!("Failed to send message. Status code: {}", response.status());
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
     }
-
-    Ok(())
 }
