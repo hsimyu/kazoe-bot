22162be5694a9bb00e79e0b1a6fa40c7343a6a08
hsimyu <hsimyu1900@gmail.com>
2024-01-16 23:37:30 +0900

    rusqlite を使うサンプルを実装.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dotenv::dotenv;
+use rusqlite::{Connection, Result};
 use serenity::{
     all::GatewayIntents,
     async_trait,
@@ -7,6 +8,12 @@ use serenity::{
 };
 use std::env;
 use tokio;
+
+#[derive(Debug)]
+struct MessageData {
+    id: i32,
+    message: String,
+}
 
 struct Handler;
 
@@ -31,18 +38,55 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    let conn = Connection::open_in_memory().expect("Failed to open memory");
 
-    let bot_token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN must be set in .env");
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    // let channel_id = env::var("TARGET_CHANNEL_ID").expect("TARGET_CHANNEL_ID must be set in .env");
+    conn.execute(
+        "CREATE TABLE messagedata (
+            id INTEGER PRIMARY KEY,
+            message TEXT NOT NULL
+        )",
+        (),
+    )
+    .expect("Failed to create inmemory db");
 
-    let mut client = Client::builder(bot_token, intents)
-        .event_handler(Handler)
-        .await
-        .expect("Error creating client");
+    let test = MessageData {
+        id: 0,
+        message: "Uho".to_string(),
+    };
 
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
+    conn.execute(
+        "INSERT INTO messagedata (id, message) VALUES (?1, ?2)",
+        (&test.id, &test.message),
+    )
+    .expect("Failed to insert message");
+
+    let mut stmt = conn.prepare("SELECT id, message FROM messagedata").unwrap();
+
+    let iter = stmt
+        .query_map([], |row| {
+            Ok(MessageData {
+                id: row.get(0)?,
+                message: row.get(1)?,
+            })
+        })
+        .unwrap();
+
+    for mes in iter {
+        println!("Message: {:?}", mes.unwrap());
     }
+
+    // dotenv().ok();
+
+    // let bot_token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN must be set in .env");
+    // let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    // // let channel_id = env::var("TARGET_CHANNEL_ID").expect("TARGET_CHANNEL_ID must be set in .env");
+
+    // let mut client = Client::builder(bot_token, intents)
+    //     .event_handler(Handler)
+    //     .await
+    //     .expect("Error creating client");
+
+    // if let Err(why) = client.start().await {
+    //     println!("Client error: {:?}", why);
+    // }
 }
