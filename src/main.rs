9c040eeb03f7836d8937934d12efcc1dba5468ec
hsimyu <hsimyu1900@gmail.com>
2024-01-17 23:54:41 +0900

    メンションで受け取った文字列を DB にインサートするところまで繋げた.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,54 @@
 use dotenv::dotenv;
 use rusqlite::{Connection, Result};
 use serenity::{
-    all::GatewayIntents,
+    all::{GatewayIntents, User},
     async_trait,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
-use std::env;
+use std::{env, sync::Arc, sync::Mutex};
 use tokio;
 
 #[derive(Debug)]
-struct MessageData {
+struct PatternRecord {
     id: i32,
-    message: String,
+    channel_id: String,
+    pattern: String,
 }
 
-struct Handler;
+struct Handler {
+    db_connection: Arc<Mutex<Connection>>,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if !msg.author.bot {
-            println!("Received message: {}", msg.content);
+            println!("Context: {:?}", ctx);
+            println!();
+            println!("Message: {}", msg.content);
+            println!("{:?}", msg);
+            println!();
 
-            if msg.content.contains("hello bot") {
-                if let Err(why) = msg.reply(&ctx.http, "Hello!").await {
+            for mentioned_user in msg.mentions.iter() {
+                println!(
+                    "- mentioned: name = {}, id = {}",
+                    mentioned_user.name, mentioned_user.id
+                );
+            }
+
+            if msg.content.contains("かぞえて") {
+                if let Err(why) = msg.reply(&ctx.http, "承知しました！").await {
                     println!("Error replying: {:?}", why);
                 }
+
+                let record = PatternRecord {
+                    id: 0,
+                    channel_id: msg.channel_id.to_string(),
+                    pattern: msg.content,
+                };
+                register_pattern(&self.db_connection.lock().unwrap(), &record);
+                dump_pattern_record(&self.db_connection.lock().unwrap());
             }
         }
     }
@@ -36,57 +58,66 @@ impl EventHandler for Handler {
     }
 }
 
-#[tokio::main]
-async fn main() {
-    let conn = Connection::open_in_memory().expect("Failed to open memory");
-
+fn create_table(conn: &Connection) {
     conn.execute(
-        "CREATE TABLE messagedata (
-            id INTEGER PRIMARY KEY,
-            message TEXT NOT NULL
+        "CREATE TABLE pattern_record (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            channel_id TEXT NOT NULL,
+            pattern TEXT NOT NULL
         )",
         (),
     )
-    .expect("Failed to create inmemory db");
+    .expect("Failed to create pattern_record");
+}
 
-    let test = MessageData {
-        id: 0,
-        message: "Uho".to_string(),
-    };
-
+fn register_pattern(conn: &Connection, record: &PatternRecord) {
     conn.execute(
-        "INSERT INTO messagedata (id, message) VALUES (?1, ?2)",
-        (&test.id, &test.message),
+        "INSERT INTO pattern_record (id, channel_id, pattern) VALUES (?1, ?2, ?3)",
+        (&record.id, &record.channel_id, &record.pattern),
     )
     .expect("Failed to insert message");
+}
 
-    let mut stmt = conn.prepare("SELECT id, message FROM messagedata").unwrap();
+fn dump_pattern_record(conn: &Connection) {
+    let mut stmt = conn
+        .prepare("SELECT id, channel_id, pattern FROM pattern_record")
+        .unwrap();
 
     let iter = stmt
         .query_map([], |row| {
-            Ok(MessageData {
+            Ok(PatternRecord {
                 id: row.get(0)?,
-                message: row.get(1)?,
+                channel_id: row.get(1)?,
+                pattern: row.get(2)?,
             })
         })
         .unwrap();
 
     for mes in iter {
-        println!("Message: {:?}", mes.unwrap());
+        println!("Pattern: {:?}", mes.unwrap());
     }
+}
 
-    // dotenv().ok();
+#[tokio::main]
+async fn main() {
+    let conn = Connection::open_in_memory().expect("Failed to open memory");
+    create_table(&conn);
 
-    // let bot_token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN must be set in .env");
-    // let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    // // let channel_id = env::var("TARGET_CHANNEL_ID").expect("TARGET_CHANNEL_ID must be set in .env");
+    dotenv().ok();
 
-    // let mut client = Client::builder(bot_token, intents)
-    //     .event_handler(Handler)
-    //     .await
-    //     .expect("Error creating client");
+    let conn = Arc::new(Mutex::new(conn));
 
-    // if let Err(why) = client.start().await {
-    //     println!("Client error: {:?}", why);
-    // }
+    let bot_token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN must be set in .env");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(bot_token, intents)
+        .event_handler(Handler {
+            db_connection: Arc::clone(&conn),
+        })
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
