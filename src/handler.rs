54c4021f1858cf6e5d42101b064592ed9f101f5d
hsimyu <hsimyu1900@gmail.com>
2024-01-22 23:59:28 +0900

    値の上書きができるようにした.

--- src/handler.rs
+++ src/handler.rs
@@ -107,14 +107,86 @@ async fn try_count_new_message(handler: &Handler, ctx: &Context, msg: &Message) 
     }
 }
 
+async fn try_set_count(handler: &Handler, ctx: &Context, msg: &Message) {
+    let channel_id = msg.channel_id.to_string();
+
+    let result = find_pattern(
+        &handler.db_connection.lock().unwrap(),
+        &channel_id,
+        &msg.content,
+    );
+
+    match result {
+        Some(pattern_record) => {
+            println!("Found: pattern = {:?}", pattern_record);
+
+            // パターンの後に来た数字で上書きする
+            let re = Regex::new(
+                format!("{}\\s+(?<count>\\d+)", pattern_record.pattern.as_str()).as_str(),
+            )
+            .unwrap();
+
+            let amount: i32 = match re.captures(&msg.content) {
+                Some(caps) => caps["count"].to_string().parse().unwrap(),
+                None => {
+                    // 失敗
+                    reply_to(
+                        &ctx,
+                        &msg,
+                        "\"うわがき [パターン] [量]\" のようにお願いしてくださいヒン",
+                    )
+                    .await;
+                    return;
+                }
+            };
+
+            println!("captured amount = {}", amount);
+
+            // 発言がマッチしていたのでカウントする
+            let count = find_count(
+                &handler.db_connection.lock().unwrap(),
+                pattern_record.id,
+                &msg.author.id.to_string(),
+            );
+
+            match count {
+                Some(mut count_record) => {
+                    println!("Found: count = {:?}", count_record);
+                    count_record.count = amount; // 加算ではなく上書きする
+                    update_count(&handler.db_connection.lock().unwrap(), &count_record);
+                    reply_to(&ctx, &msg, format!("{}", count_record.count).as_str()).await;
+                }
+                None => {
+                    register_new_count(
+                        &handler.db_connection.lock().unwrap(),
+                        &CountRecord {
+                            id: 0,
+                            pattern_id: pattern_record.id,
+                            user_id: msg.author.id.to_string(),
+                            count: amount,
+                        },
+                    );
+                    reply_to(&ctx, &msg, format!("{}", amount).as_str()).await;
+                }
+            }
+        }
+        None => return, // 何もしない
+    }
+}
+
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if !msg.author.bot {
             println!("Message: {}", msg.content);
 
+            // TODO: bot へのメンションを区別できるようにする
+
             if msg.content.contains("かぞえて") {
                 register_new_pattern(&self, &ctx, &msg).await;
+            } else if msg.content.contains("うわがき") {
+                // 設定値を更新する
+                try_set_count(&self, &ctx, &msg).await;
             } else {
                 // 登録依頼ではないので数えてみる
                 try_count_new_message(&self, &ctx, &msg).await;
